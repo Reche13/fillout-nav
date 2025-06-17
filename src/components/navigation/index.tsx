67c2cbb8be95410073f555718c7be599da5d312d
Reche Soares <rechesoares13@gmail.com>
2025-06-17 20:01:27 +0530

    setup basic drag and drop context

--- src/components/navigation/index.tsx
+++ src/components/navigation/index.tsx
@@ -1,5 +1,51 @@
 "use client";
 
+import {
+  closestCenter,
+  DndContext,
+  DragEndEvent,
+  UniqueIdentifier,
+} from "@dnd-kit/core";
+import { restrictToHorizontalAxis } from "@dnd-kit/modifiers";
+import {
+  arrayMove,
+  horizontalListSortingStrategy,
+  SortableContext,
+} from "@dnd-kit/sortable";
+import React, { useState } from "react";
+
 export const Navigation = () => {
-  return <div>Navigation</div>;
+  const [items, setItems] = useState<{ id: UniqueIdentifier; label: string }[]>(
+    () => [
+      { id: "1", label: "Item 1" },
+      { id: "2", label: "Item 2" },
+      { id: "3", label: "Item 3" },
+      { id: "4", label: "Item 4" },
+      { id: "5", label: "Item 5" },
+    ]
+  );
+
+  const handleDragEnd = (event: DragEndEvent) => {
+    const { active, over } = event;
+    if (over && active.id !== over.id) {
+      const oldIndex = items.findIndex((i) => i.id === active.id);
+      const newIndex = items.findIndex((i) => i.id === over.id);
+      setItems(arrayMove(items, oldIndex, newIndex));
+    }
+  };
+
+  return (
+    <DndContext
+      collisionDetection={closestCenter}
+      onDragEnd={handleDragEnd}
+      modifiers={[restrictToHorizontalAxis]}
+    >
+      <SortableContext
+        items={items.map((i) => i.id)}
+        strategy={horizontalListSortingStrategy}
+      >
+        <div className="flex items-center"></div>
+      </SortableContext>
+    </DndContext>
+  );
 };
