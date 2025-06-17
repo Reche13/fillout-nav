f7c5d40c1e62fccaa8efc21cd1e917dc4bf196ca
Reche Soares <rechesoares13@gmail.com>
2025-06-17 14:29:17 +0530

    add dummy page

--- src/components/page/index.tsx
+++ src/components/page/index.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export const Page = () => {
-  return <div>Page</div>;
+  return (
+    <div className="w-full max-w-[300px] h-[300px] bg-background-pri rounded-xl shadow-fillout flex items-center justify-center">
+      Page
+    </div>
+  );
 };
