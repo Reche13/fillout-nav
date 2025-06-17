bfa1b6819733214260a54258faed1cba9222e2dd
Reche Soares <rechesoares13@gmail.com>
2025-06-17 14:20:24 +0530

    create page + nav components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
+import { Page } from "@/components/page";
+import { Navigation } from "@/components/navigation";
+
 export default function Home() {
-  return <main></main>;
+  return (
+    <main className="w-full h-screen bg-space py-16 px-10 flex flex-col items-center">
+      <Page />
+      <Navigation />
+    </main>
+  );
 }
